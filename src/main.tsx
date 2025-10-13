86f28bd8a5da4773d0765003a1c10b79233f6bf1
Abdulaziz Abdukhalimov <abdukhalimov_front_end@Abdulazizs-MacBook-Pro.local>
2025-10-13 23:29:45 +0900

    fix: Initial setup correction

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React  from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import './index.css'
-import App from './app/App.tsx'
-import {store} from './app/store' // Import  Redux store
+import "./css/index.css";
+import App from "./app/App";
+import { store } from "./app/store"; // Import Redux store
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
